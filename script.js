3f844ba3861fb4f15ccb82336c1d90b2533ebdab
Dorasanaiah Kolla <nani2002119@gmail.com>
2024-01-17 14:07:43 +0530

    file updated

---
+++ script.js
@@ -0,0 +1,111 @@
+
+
+const knowledgeButton = document.querySelector(".knowledgeButton");
+const tradingButton = document.querySelector(".tradingButton");
+const buttonContainer = document.querySelector(".buttonContainer");
+const backButtonDiv = document.querySelector(".backButton");
+const backButton = document.querySelector("#backButton");
+const mindButton = document.querySelector(".mindButton");
+const quote = document.querySelector(".quote");
+const refresh = document.querySelector(".refresh");
+const selectValue = document.querySelector("#contentType");
+const quoteInput=document.querySelector(".quoteInput")
+const quoteMessage=document.querySelector(".quoteMessage")
+function getValue() {
+  
+  if (selectValue.value == "Knowledge") {
+    addQuote("KNOWLEDGEQUOTES")
+  } else if (selectValue.value == "Trading") {
+    addQuote("TRADINGQUOTES")
+  } else if(selectValue.value=="Mind") {
+    addQuote("MINDQUOTES")
+  } 
+  quoteInput.style.display="none";
+  quoteMessage.style.display="flex";
+}
+function addQuote(tableName) {
+  const newQuote = document.querySelector("#newQuote").value;
+
+  if (!newQuote) {
+      console.error('New quote is required.');
+      return;
+  }
+
+  fetch('/addQuote', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          tableName: tableName,
+          quote: newQuote,
+      }),
+  })
+  .then(response => response.json())
+  .then(data => {
+      console.log('Quote added successfully:', data);
+      
+  })
+  .catch(error => {
+      console.error('Error adding quote:', error);
+  });
+}
+
+
+
+
+let quoteType = 0;
+tradingButton.addEventListener('click', () => {
+  quoteType = 1;
+  getRandomQuote("TRADINGQUOTES")
+});
+mindButton.addEventListener('click', () => {
+  quoteType = 2;
+  getRandomQuote("MINDQUOTES")
+}
+);
+knowledgeButton.addEventListener("click", function () {
+  quoteType = 0;
+  getRandomQuote("KNOWLEDGEQUOTES")
+}
+);
+refresh.addEventListener("click", function () {
+  if (quoteType === 0) {
+    refreshQuote("KNOWLEDGEQUOTES");
+  } else if (quoteType === 1) {
+    refreshQuote("TRADINGQUOTES");
+  } else {
+    refreshQuote("MINDQUOTES");
+  }
+});
+
+function getRandomQuote(tableName){
+  fetch(`/getRandomQuote?tableName=${tableName}`)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.quote);
+      displayQuote(data.quote[0]["QUOTES"]);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    }
+    );
+}
+
+
+backButton.addEventListener("click", function () {
+  buttonContainer.style.display = "flex";
+  quote.style.display = "none";
+  backButtonDiv.style.display = "none";
+});
+function refreshQuote(tableName) {
+  getRandomQuote(tableName)
+};
+
+
+function displayQuote(quoteText) {
+  buttonContainer.style.display = "None";
+  quote.style.display = "Block";
+  quote.textContent = " ' " + quoteText + " ' ";
+  backButtonDiv.style.display = "flex";
+}
---
+++ server.js
@@ -0,0 +1,122 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+const snowflake = require('snowflake-sdk');
+const { table, log } = require('console');
+const connection = snowflake.createConnection({
+    account: "qkb03942.us-east-1", // Replace with your Snowflake account URL (including the region and account name)
+    username: "DORA",
+    password: "D@ra1192002",
+    warehouse: "COMPUTE_WH",
+    database: "QUOTES",
+    schema: "PUBLIC"
+});
+
+connection.connect(function(err, conn) {
+    if (err) {
+        console.error('Unable to connect: ' + err.message);
+    } else {
+        // Optional: store the connection ID.
+        var connection_ID = conn.getId();
+        console.log('Connected to Snowflake. Connection ID:', connection_ID);
+    }
+});
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/script.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'script.js'));
+});
+app.get('/getRandomQuote', (req, res) => {
+    const tableName = req.query.tableName;
+
+    if (!tableName) {
+        return res.status(400).json({ error: 'Table name is required.' });
+    }
+
+    // Example: Retrieve a random quote from Snowflake and send it to the client
+    connection.execute({
+        sqlText: "SELECT * FROM "+tableName+"  ORDER BY RANDOM() LIMIT 1",
+        complete: function (err, stmt, rows) {
+            if (err) {
+                console.error('Failed to execute statement due to the following error: ' + err.message);
+                res.status(500).send('Internal Server Error');
+            } else {
+                res.json({ quote: rows });
+            }
+        }
+    });
+});
+app.post('/addQuote', async (req, res) => {
+    const tableName = req.body.tableName;
+    const quote = req.body.quote;
+    console.log(tableName, quote);
+
+    if (!tableName || !quote) {
+        return res.status(400).json({ error: 'Table name and quote are required.' });
+    }
+    let id = 0;
+
+    connection.execute({
+        sqlText: "SELECT MAX(ID) AS maxId FROM " + tableName,
+        complete: function (err, stmt, rows) {
+            if (err) {
+                console.error('Failed to execute statement due to the following error: ' + err.message);
+                return res.status(500).send('Internal Server Error');
+                console.log(rows);
+            } else {
+                id = rows[0]["MAXID"] || 0;
+                id++;
+                // Insert the new quote with the incremented ID
+                connection.execute({
+                    sqlText: "INSERT INTO " + tableName + " (ID, QUOTES) VALUES (?, ?)",  // Replace QUOTES_COLUMN with the actual column name
+                    binds: [id, quote],
+                    complete: function (err, stmt, rows) {
+                        if (err) {
+                            console.error('Failed to execute statement due to the following error: ' + err.message);
+                            return res.status(500).send('Internal Server Error');
+                        } else {
+                            res.json({ success: true });
+                            
+                        }
+                    }
+                });
+            }
+        }
+    });
+    
+});
+app.delete('/deleteQuote' ,async (req, res) => {
+    const tableName = req.body.tableName;
+    const quote = req.body.quote;
+    if (!tableName || !quote) {
+        return res.status(400).json({ error: 'Table name and quote are required.' });
+    }
+    connection.execute({
+        sqlText: `DELETE FROM ${tableName} WHERE ID = ?`,
+        binds: [quoteId],
+        complete: function (err, stmt, rows) {
+            if (err) {
+                console.error('Failed to execute statement due to the following error: ' + err.message);
+                return res.status(500).send('Internal Server Error');
+            } else {
+                res.json({ success: true });
+            }
+        }
+    });
+}
+)
+
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+});
